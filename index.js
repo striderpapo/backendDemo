ed6a3d9e1e296d845817cd0d394a836ec89bd6b4
Arturo González <arturogarza.gonz@hotmail.com>
2024-01-18 19:57:22 -0600

    agregado elnombre dela base en atlas

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var port = 3700;
 
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/demo', {/*useUnifiedTopology: true, useNewUrlParser: true*/})
-mongoose.connect('mongodb+srv://striper:ZQvSE%40tiZ%234X6Km@cluster0.yr5aoiu.mongodb.net/?retryWrites=true&w=majority', {/*useUnifiedTopology: true, useNewUrlParser: true*/})
+mongoose.connect('mongodb+srv://striper:ZQvSE%40tiZ%234X6Km@cluster0.yr5aoiu.mongodb.net/demo?retryWrites=true&w=majority', {/*useUnifiedTopology: true, useNewUrlParser: true*/})
 	.then(()=>{
 		console.log("Conexion a la base de datos establecida satisfactoriamente");
 		//creacion del servidor
