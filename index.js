f43f106ececdc94df35abf39c7956493ac978025
Arturo González <arturogarza.gonz@hotmail.com>
2024-01-18 20:34:58 -0600

    segundamodificacion index para conectar concluster

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ var port = 3700;
 
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/demo', {/*useUnifiedTopology: true, useNewUrlParser: true*/})
-mongoose.connect('mongodb+srv://striper:ZQvSE%40tiZ%234X6Km@cluster0.yr5aoiu.mongodb.net/demo?retryWrites=true&w=majority', {/*useUnifiedTopology: true, useNewUrlParser: true*/})
+mongoose.connect('mongodb+srv://striper:ZQvSE%40tiZ%234X6Km@cluster0.yr5aoiu.mongodb.net/demo?retryWrites=true&w=majority')
 	.then(()=>{
 		console.log("Conexion a la base de datos establecida satisfactoriamente");
 		//creacion del servidor
-		app.listen(port,()=>{
+		/*app.listen(port,()=>{
 			console.log("Servidor corriendo correctamente en la url localhost:3700");
-		});
+		});*/
 	})
 	.catch(err =>console.log("error al conectarse a la base:"+err));
