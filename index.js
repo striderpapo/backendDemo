379f0989769cf4cb4c39734b3c528a60e87b09cd
Arturo González <arturogarza.gonz@hotmail.com>
2024-01-19 17:26:57 -0600

    agregado tls en la conexion

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ var port = 3700;
 
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/demo', {/*useUnifiedTopology: true, useNewUrlParser: true*/})
-mongoose.connect('mongodb+srv://striper:ZQvSE%40tiZ%234X6Km@cluster0.6ma1n4c.mongodb.net/demo?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://striper:ZQvSE%40tiZ%234X6Km@cluster0.6ma1n4c.mongodb.net/demo?retryWrites=true&w=majority', {
+	tls: true,
+  })
 	/*.then(()=>{
 		console.log("Conexion a la base de datos establecida satisfactoriamente");
 		//creacion del servidor
-		/*app.listen(port,()=>{
+		app.listen(port,()=>{
 			console.log("Servidor corriendo correctamente en la url localhost:3700");
 		});
 	})
