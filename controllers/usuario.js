9381ca71b42f1c91bb6da155e4344d89ee7afe05
Arturo González <arturogarza.gonz@hotmail.com>
2024-07-30 19:07:03 -0600

    funcion login agregada

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -6,6 +6,7 @@ var path=require('path');
 const cloudinary = require('cloudinary').v2;
 var bcrypt=require('bcrypt');
 var jwt=require('jsonwebtoken');
+const https = require('https');
 
 var controller = {
 	saveUsuario:function(req,res){
@@ -14,6 +15,7 @@ var controller = {
 		var params=req.body;
 		console.log(params)
 		usuario.username = params.username.toLowerCase();
+		usuario.facebook_id = null
 		//usuario.password = params.password.toLowerCase();
 		const encryptedPassword =  bcrypt.hashSync(params.password, 10);
 		usuario.password = encryptedPassword;
@@ -49,6 +51,97 @@ var controller = {
 .catch(function (err) {
   if(err) return res.status(500).send({message:"Error al guardar al usuario"});
 });
+	},
+	saveUsuarioFace:function(req,res){
+		var usuario = new Usuario();
+
+		const { accessToken }=req.body;
+		console.log(req.body)
+		console.log(accessToken)
+		const url = `https://graph.facebook.com/v12.0/me?access_token=${accessToken}&fields=id,name,picture`;
+		//console.log(url)
+		https.get(url, (resp) => {
+			let data = '';
+		
+			// Un trozo de datos ha sido recibido.
+			resp.on('data', (chunk) => {
+			  data += chunk;
+			  console.log(typeof data)
+			})
+
+			resp.on('end', () => {
+				if (resp.statusCode === 200) {
+				  const user = JSON.parse(data);
+		  
+					console.log(user.name)
+
+					usuario.username = user.name.toLowerCase();
+					usuario.password = null;
+					usuario.facebook_id = accessToken
+					const token = jwt.sign(
+				  {_id: usuario._id,username:usuario.username},
+				  "Secret_key123",
+				  {}
+				);
+			
+					usuario.token=token;
+				  // Aquí podrías crear una sesión o un token JWT
+				  usuario.save()
+				  .then(function (usuarioStored) {
+			if(!usuarioStored) return res.status(404).send({message:"No se ha podido guardar al usuario"});
+			cloudinary.api.create_folder(usuarioStored._id, (error, result) => {
+			if (error) {
+			  console.error('Error al crear la carpeta en Cloudinary:', error);
+			} else {
+			  console.log('Carpeta creada exitosamente:', result);
+			  return res.status(200).send({usuario:usuarioStored});
+			}
+		  });
+		  })
+		  .catch(function (err) {
+			if(err) return res.status(500).send({message:"Error al guardar al usuario"});
+		  });
+				} else {
+				  res.status(400).json({ message: 'Token de acceso no válido', error: data });
+				}
+			  });
+		});
+/*		usuario.username = params.username.toLowerCase();
+		//usuario.password = params.password.toLowerCase();
+		const encryptedPassword =  bcrypt.hashSync(params.password, 10);
+		usuario.password = encryptedPassword;
+		const token = jwt.sign(
+      {_id: usuario._id,username:usuario.username},
+      "Secret_key123",
+      {}
+    );
+
+   		usuario.token=token;
+		
+		/*usuario.save((err,usuarioStored)=>{
+			if(err) return res.status(500).send({message:"Error al guardar al usuario"});
+
+			if(!usuarioStored) return res.status(404).send({message:"No se ha podido guardar al usuario"});
+
+			return res.status(200).send({usuario:usuarioStored});
+		});
+
+
+		usuario.save()
+		.then(function (usuarioStored) {
+  if(!usuarioStored) return res.status(404).send({message:"No se ha podido guardar al usuario"});
+  cloudinary.api.create_folder(usuarioStored._id, (error, result) => {
+  if (error) {
+    console.error('Error al crear la carpeta en Cloudinary:', error);
+  } else {
+    console.log('Carpeta creada exitosamente:', result);
+    return res.status(200).send({usuario:usuarioStored});
+  }
+});
+})
+.catch(function (err) {
+  if(err) return res.status(500).send({message:"Error al guardar al usuario"});
+});*/
 	},
 	async getUsuario(req,res){
 		try {
--- routes/usuario.js
+++ routes/usuario.js
@@ -9,6 +9,7 @@ var routerUsuario = express.Router();
 
 routerUsuario.post('/suser',UsuarioController.saveUsuario);
 routerUsuario.post('/guser',UsuarioController.getUsuario);
+routerUsuario.post('/sfuser',UsuarioController.saveUsuarioFace);
 
 
 
